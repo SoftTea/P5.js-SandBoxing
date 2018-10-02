d356de3ad6aed75b4208210dbce96430b9858cb0
Tim Mickle <TimothyRMickle@gmail.com>
2018-10-01 23:42:43 -0500

    primrose

--- 9-29 Chain-Classes/main.js
+++ 9-29 Chain-Classes/main.js
@@ -1,15 +1,16 @@
 
 let circles = []
 let colors = {
-    primary: [255,212,229],
-    second: [212,255,234],
-    third: [238,203,255],
-    background: [254,255,163 ] 
+    primary: [0,0,0],
+    second: [255,0,50],
+    third: [255],
+    background: [255 ] 
 }
 
 
 function setup () {
     createCanvas (600,500)
+    angleMode(DEGREES)
     for (let i= 0; i <40; i++) {
         circles.push ( new Circle (-i*20,0,45,colors.primary ) )
 
@@ -36,21 +37,28 @@ function draw () {
     // let circle = new Circle (0, 40, 20)
     // circle.draw
     noFill()
-    stroke(colors.background);
-    strokeWeight(15);
+    stroke(colors.second);
+    strokeWeight(5);
+    
     // noStroke();
     push ();
+    
     translate(width/2, height/2)
     // ellipse (0, 70 , 60)
     // ellipse (35, 70 , 60)
     // ellipse (-60, 70 , 60)
+    
     for (let i = 0 ; i < circles.length; i++) {
+        
+        rotate (90)
+        
         fill (circles[i].color  )
         // fill (random(1,25) )
         circles[i].draw()
         circles[i].move();
        
     }
+    
 
     pop ();
     
@@ -60,15 +68,15 @@ class Circle {
     constructor (x,y,r,color) {
 this.x =x ,
 this.y = y,
-this.r = r  ,
+this.r = r *5  ,
 this.range = [x,y,r]
-this.rate = 5;
+this.rate = 1;
 this.color = color
 
 
     }
    draw () {
-    ellipse (this.x,this.y,this.r)
+    ellipse (this.x,this.y,this.r )
    }
 
 
@@ -86,18 +94,38 @@ this.color = color
     //         this.rate = 0
     //     } 
 
-        if ( this.range[0] < this.x ) {
-            this.x = this.x - random(2,15)
-            this.r = this.r
-            // this.r--
+        // if ( this.range[0] < this.x ) {
+        //     this.x = this.x --
+            
+        //     // this.r--
            
-         } 
-         if ( this.r > this.range[2] * 14) {
-             this.rate = -10
+        //  } 
+         if ( this.r > this.range[2] * 20) {
+             this.rate = -1
+            
+            //  if (this.color == colors.primary) {
+            //     this.color = colors.second
+            // } else if (this.color == colors.second) {
+            //     this.color = colors.third
+            // }else if (this.color == colors.third) {
+            //     this.color = colors.background
+            // } else if (this.color == colors.background) {
+            //     this.color = colors.primary
+            // } 
              
             
-         } else if (this.r < this.range[2]   ) {
-            this.rate = 5
+         } 
+         else if (this.r < this.range[2] *5 ) {
+            this.rate = 1
+            // if (this.color == colors.primary) {
+            //     this.color = colors.second
+            // } else if (this.color == colors.second) {
+            //     this.color = colors.third
+            // }else if (this.color == colors.third) {
+            //     this.color = colors.background
+            // } else if (this.color == colors.background) {
+            //     this.color = colors.primary
+            // } 
           
 
          }
@@ -107,21 +135,14 @@ this.color = color
         //     // this.r ++
           
         // }
-        this.x ++
+        // this.x += -this.rate /100
         
-        this.r+=this.rate 
+        
+        this.r+=this.rate * 2
         
         // this.x = this.x + this.rate
     }
 }
 
 
-// if (this.color == colors.primary) {
-//     this.color = colors.second
-// } else if (this.color == colors.second) {
-//     this.color = colors.third
-// }else if (this.color == colors.third) {
-//     this.color = colors.background
-// } else if (this.color == colors.background) {
-//     this.color = colors.primary
-// } 
+
